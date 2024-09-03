96798384c37339439f244ec9ec5b81c385890c9f
Sekar, Anantharaman <Anantharaman.Sekar@philips.com>
2024-09-03 09:41:36 +0530

    Decomp2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 #include <assert.h>
-#include "ColorPair.h"
-
-namespace TelCoColorCoder
-{
-    ColorPair GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = 
-            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-    }
-
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
-    }
-}
+#include "Globals.h"
 
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
